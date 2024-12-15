38bb5bbb229294a83f933b07226a63085d85a4d7
Kire Radinski <kire.volkot@gmail.com>
2024-12-15 18:45:55 +0200

    Remove symfony/dotenv from production and restrict to dev environment

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,7 +3,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-// If APP_ENV is already set (injected by SymfonyCloud), skip loading the .env file
-if (!isset($_SERVER['APP_ENV']) && class_exists(Dotenv::class)) {
+// Load .env ONLY in local development
+if (class_exists(Dotenv::class) && !isset($_SERVER['APP_ENV'])) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
