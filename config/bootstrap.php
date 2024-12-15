4804e24867e013b2dd75ca78efc9abfa88f97cc7
Kire Radinski <kire.volkot@gmail.com>
2024-12-15 17:38:46 +0200

    Fix environment variable loading for production

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,6 +3,9 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (class_exists(Dotenv::class)) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+// Check if .env.local.php exists for local overrides; otherwise, rely on injected environment variables
+if (file_exists(dirname(__DIR__).'/.env')) {
+    require dirname(__DIR__).'/.env';
+} elseif (class_exists(Dotenv::class)) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env', 'APP_ENV', 'prod');
 }
