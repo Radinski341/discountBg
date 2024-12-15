8c237567c758644e3e5f4722b2a7f854646ce921
Kire Radinski <kire.volkot@gmail.com>
2024-12-15 19:01:04 +0200

    Deploy with .env file commited

--- config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Symfony\Component\Dotenv\Dotenv;
-
-require dirname(__DIR__).'/vendor/autoload.php';
-
-// Load .env ONLY in local development
-if (class_exists(Dotenv::class) && !isset($_SERVER['APP_ENV'])) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
