b0ae0e9aea3dff4bf8688bca2c8c351a0a941674
Kire Radinski <kire.volkot@gmail.com>
2024-12-26 12:32:32 +0200

    Register page bug fixed

--- src/Form/Public/RegisterFormType.php
+++ src/Form/Public/RegisterFormType.php
@@ -52,7 +52,9 @@ class RegisterFormType extends AbstractType
                 ]
             ])
             ->add('city', TextType::class, [
-                'placeholder' => 'Град',
+                'attr' => [
+                    'placeholder' => 'Град'
+                ],
                 'required' => true,
                 'label' => 'Град'
             ])
